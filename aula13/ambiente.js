d674b05a9fe6cce60c16567a5b4d706e1e72cafb
gabrielpolidoro <98493279+GabrielPolidoro@users.noreply.github.com>
2022-03-21 10:57:47 +0000

    Do while

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,14 @@
 var c = 1
-while (c <= 6) {
-    console.log('Tudo bem?')
+while (c <= 5) {
+    console.log(`Contando número ${c}`)
     c++
 }
+
+                        //OU
+
+var c = 10
+do {
+    console.log(`Contando número ${c}`)
+    c++
+} while (c <= 15)
+
