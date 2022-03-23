9e1bb6063db761bb637d9975ffb59b742c4eb664
gabrielpolidoro <98493279+GabrielPolidoro@users.noreply.github.com>
2022-03-23 15:37:33 +0000

    att

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,16 +6,30 @@ function contar() {
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     }
     else{
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} \u{1F603}`
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
-    }
 
+        if (i < f){
+            // contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }   
+        }    
+        else{
+            // contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`  
+    }
 }
