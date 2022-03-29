5720c1c2ab729b2678162a3d66c762688652fca2
gabrielpolidoro <98493279+GabrielPolidoro@users.noreply.github.com>
2022-03-29 09:28:35 +0100

    tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,15 +1,18 @@
-function calcular() {
+function tabuada() {
     let num = document.getElementById('txtn')
-    let area = document.getElementById('txta')
-}
-if (num.value.length == 0) {
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
     alert('Por favor, digite um número!')
-}
-else {
-    let num = Number(num.value)
-    let area = Number(area.value)
-
-    for(let c = 1; c <= 10; c++) {
-        area.innerHTML +=  `${num} x ${c} = ${num*c}`
+    }
+    else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option') // criando a tag html option dentro do select
+            item.text = `${n} x ${c} = ${n*c}` // numero vezes o contador
+            tab.appendChild(item) // adicionar um elemento filho que vai ser o item
+            c++
+        }
     }
 }
