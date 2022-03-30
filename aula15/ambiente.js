fa45340fb9d1b7211aa280785a41da5038537c88
gabrielpolidoro <98493279+GabrielPolidoro@users.noreply.github.com>
2022-03-30 10:10:56 +0100

    array com for

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,6 +1,12 @@
 let num = [5, 8, 2, 9, 3]
+num.push(1) // colocar o valor 1 na última posição no vetor
+num.sort() // organizar os valores em ordem crescente
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`) // tamanho do vetor
+console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-console.log(`Nosso vetor é o ${num}`)
 
-console.log(`O vetor tem ${num.length} posições`)
-console.log(`O primeiro valor do vetor é ${[0]}`)
+// posição 0, enquanto a pos for menor que o tamanho de num, adiciona uma pos++ para aumentar até chegar na última posição do vetor. 
+for(let pos = 0; pos < num.length; pos++) {
+    console.log(num[pos])
+}
