c446da59eed50bc0b8576d0891ff6962136bc86e
gabrielpolidoro <98493279+GabrielPolidoro@users.noreply.github.com>
2022-04-02 19:55:18 +0100

    indexOf

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -10,3 +10,6 @@ console.log(`O primeiro valor do vetor é ${num[0]}`)
 for(let pos = 0; pos < num.length; pos++) {
     console.log(num[pos])
 }
+
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
